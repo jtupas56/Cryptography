81074021e58e516cf6beb2d67e4a9bd7e9398a98
Domas <domasbraz@gmail.com>
2024-11-08 21:06:54 +0000

    adjusted format

--- src/main/java/com/mycompany/cryptography_project/Records/JSONFormatter.java
+++ src/main/java/com/mycompany/cryptography_project/Records/JSONFormatter.java
@@ -1,26 +1,24 @@
 package com.mycompany.cryptography_project.Records;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class JSONFormatter 
 {
-    private static String dir = "src/main/java/com/mycompany/cryptography_project/Records/data/";
-    
-    public static ArrayList getJSONData() throws IOException
+
+    public static ArrayList<ArrayList<String[]>> parseList(String data)
     {
-        String data = FileReadWrite.readFromFile( dir + "patient_medical_records.json");
-        data = data.replaceAll("\\[\r\n    \\{\r\n        ", "");
-        data = data.replaceAll("\r\n    \\}\r\n\\]\r\n", "");
+        
+        data = data.replaceAll("\\[\n    \\{", "");
+        data = data.replaceAll("    \\}\n\\]", "");
         data = data.replaceAll("        ", "");
-        String rows[] = data.split("\r\n    \\},\r\n    \\{\r\n");
+        String rows[] = data.split("\n    \\},\n    \\{\n");
 
         ArrayList<ArrayList<String[]>> parsedData = new ArrayList<>();
 
         for (String row : rows)
         {
             ArrayList<String[]> field = new ArrayList<>();
-            String details[] = row.split(",\r\n\"");
+            String details[] = row.split(",\n\"");
 
             for (String detail : details)
             {
@@ -35,17 +33,4 @@ public class JSONFormatter
         return parsedData;
     }
 
-    public static void main(String[] args) 
-    {
-        try
-        {
-
-            getJSONData();
-            
-        }
-        catch (IOException e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
 }
