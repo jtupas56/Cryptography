0ffc6dde202121b9f4319254a2f74c47ceeb7f44
Domas <domasbraz@gmail.com>
2024-11-08 21:04:36 +0000

    fixed encrypted data issues

--- src/main/java/com/mycompany/cryptography_project/Records/data/WriteEncryptedData.java
+++ src/main/java/com/mycompany/cryptography_project/Records/data/WriteEncryptedData.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import com.mycompany.cryptography_project.Records.FileReadWrite;
 import com.mycompany.cryptography_project.Records.RSAEncryption;
+import java.nio.charset.StandardCharsets;
 
 public class WriteEncryptedData 
 {
@@ -14,18 +15,19 @@ public class WriteEncryptedData
             //System.out.println(System.getProperty("user.dir"));
             String dir = "src/main/java/com/mycompany/cryptography_project/Records/data/";
             
-            String jsonString = FileReadWrite.readFromFile( dir + "patient_medical_records.json");
+            String jsonString = new String(FileReadWrite.readFromFile( dir + "patient_medical_records.json"));
             RSAEncryption rsa = new RSAEncryption();
             ArrayList<String> listOfData = rsa.longStringToList(jsonString);
 
             ArrayList<byte[]> encryptedListBytes = rsa.encryptList(listOfData);
 
             String encryptedData = rsa.byteListToString(encryptedListBytes);
-
-            FileReadWrite.writeToFile( dir + "encryptedData.txt", encryptedData);
+           
+            FileReadWrite.writeToFile( dir + "encryptedData.txt", encryptedData.getBytes(StandardCharsets.ISO_8859_1));
 
             String privateKey = rsa.getPrivateKey();
-            FileReadWrite.writeToFile( dir + "privateKey.txt", privateKey);
+
+            FileReadWrite.writeToFile( dir + "privateKey.txt", privateKey.getBytes(StandardCharsets.ISO_8859_1));
 
             System.out.println("write successful");
         }
