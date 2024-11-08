3240c76c17a715339deefa1ec6e8087960b5ae04
Domas <domasbraz@gmail.com>
2024-11-08 14:24:58 +0000

    added encrypted data

---
+++ src/main/java/com/mycompany/cryptography_project/Records/data/WriteEncryptedData.java
@@ -0,0 +1,45 @@
+package com.mycompany.cryptography_project.Records.data;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import com.mycompany.cryptography_project.Records.FileReadWrite;
+import com.mycompany.cryptography_project.Records.RSAEncryption;
+
+public class WriteEncryptedData 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            //System.out.println(System.getProperty("user.dir"));
+            String dir = "src/main/java/com/mycompany/cryptography_project/Records/data/";
+            
+            String jsonString = FileReadWrite.readFromFile( dir + "patient_medical_records.json");
+            RSAEncryption rsa = new RSAEncryption();
+            ArrayList<String> listOfData = rsa.longStringToList(jsonString);
+
+            ArrayList<byte[]> encryptedListBytes = rsa.encryptList(listOfData);
+
+            String encryptedData = rsa.byteListToString(encryptedListBytes);
+
+            FileReadWrite.writeToFile( dir + "encryptedData.txt", encryptedData);
+
+            String privateKey = rsa.getPrivateKey();
+            FileReadWrite.writeToFile( dir + "privateKey.txt", privateKey);
+
+            System.out.println("write successful");
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            System.exit(0);
+        }
+    }    
+}
